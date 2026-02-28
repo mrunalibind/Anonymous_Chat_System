620f51ac7dafb4fef4ad0ba00b7c05cd6461e7d8
Mrunali Bind <mrunalibind123@gmail.com>
2026-02-28 09:54:31 +0530

    URL CHange for production

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:7200"; // change later for production
+const SOCKET_URL = "https://taptalent-backend.onrender.com"; // change later for production
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
